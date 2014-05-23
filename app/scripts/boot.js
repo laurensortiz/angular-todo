afb90d81f292572d561dced9acd7d830a737d1ac
Alejandro Nanez <alejonanez@gmail.com>
2014-05-23 18:46:11 -0500

    Add localstorage polyfill

--- app/scripts/boot.js
+++ app/scripts/boot.js
@@ -10,6 +10,7 @@
     { require: 'bower_components/requirejs/require.js' },
     { modernizr: 'bower_components/modernizr/modernizr.js' },
     { jQuery: 'bower_components/jquery/dist/jquery.js' },
+    { yepnope: 'bower_components/yepnope/yepnope.js' },
     { angular: 'bower_components/angular/angular.js' },
     { ngRoute: 'bower_components/angular-route/angular-route.min.js' },
     { ngResource: 'bower_components/angular-resource/angular-resource.min.js' },
@@ -25,6 +26,12 @@
 
   // Excecute when DOM ready
   head.ready('ALL', function () {
+
+    yepnope({
+        test: Modernizr.localstorage,
+        nope: 'scripts/polyfill/localstorage.js'
+    })
+
     require.config({
         baseUrl: './scripts',
         paths: {
